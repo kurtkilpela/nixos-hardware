c5279c74ada5eba8e0c7a41bf910b90aebb9e672
Kurt Kilpela <dev@kurtkilpela.com>
2020-11-03 17:24:17 -0800

    Removed graphics options from 11-5

---
+++ apple/macbook-pro/11-4/default.nix
@@ -0,0 +1,22 @@
+{ lib, config, pkgs, ... }:
+
+let
+
+  kernelPackages = config.boot.kernelPackages;
+
+in
+
+{
+  imports = [
+    ../.
+    ../../../common/pc/laptop/ssd
+    <nixpkgs/nixos/modules/hardware/network/broadcom-43xx.nix>
+  ];
+
+  services.udev.extraRules =
+    # Disable XHC1 wakeup signal to avoid resume getting triggered some time
+    # after suspend. Reboot required for this to take effect.
+    lib.optionalString
+      (lib.versionAtLeast kernelPackages.kernel.version "3.13")
+      ''SUBSYSTEM=="pci", KERNEL=="0000:00:14.0", ATTR{power/wakeup}="disabled"'';
+}
